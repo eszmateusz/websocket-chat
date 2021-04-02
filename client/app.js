38536eedcf1ee48089b47d0e37378df7d35dce46
eszmateusz <71881067+eszmateusz@users.noreply.github.com>
2021-04-02 21:49:14 +0200

    Add client files. Init server. Add basic functionalities.

---
+++ client/app.js
@@ -0,0 +1,58 @@
+let userName = '';
+
+const loginForm = document.getElementById('welcome-form');
+const messagesSection = document.getElementById('messages-section');
+const messagesList = document.getElementById('messages-list');
+const addMessageForm = document.getElementById('add-messages-form');
+const userNameInput = document.getElementById('username');
+const messageContentInput = document.getElementById('message-content');
+
+// LOGIN //
+
+loginForm.addEventListener('submit', function(event) {
+  event.preventDefault();
+  login();
+});
+
+function login() {
+  const loginValue = userNameInput.value;
+
+  if (loginValue) {
+    userName = loginValue;
+    loginForm.classList.remove('show');
+    messagesSection.classList.add('show');
+  } else {
+    window.alert('You should enter your name first');
+  }
+};
+
+// MESSAGES //
+
+addMessageForm.addEventListener('submit', function(event) {
+  event.preventDefault();
+  sendMessage(userName, messageContentInput.value)
+})
+
+function sendMessage (userName, textField) {
+  if (textField) {
+    addMessage(userName, textField);
+    messageContentInput.value = '';
+  } else {
+    window.alert('Please type your message')
+  }
+}
+
+function addMessage(author, content) {
+  const message = document.createElement('li');
+
+  message.classList.add('message message--received');
+
+  if (author === userName) {
+    message.classList.add('message--self');
+  }
+  message.innerHTML = `
+    <h3 class="message__author">${userName === author ? 'You' : author }</h3>
+    <div class="message__content">${content}</div>
+  `;
+  messagesList.appendChild(message);
+};
